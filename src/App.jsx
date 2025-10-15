3752ed3326039cc4ecb90f45e1beb6d2a02926c4
DA-Quintero <diego.quintero@uao.edu.co>
2025-10-15 11:32:37 -0500

    Entrega challenge #11

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Registro } from './components/register'
+import { Login } from './components/login'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/register" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/register" replace />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBge59jNt92-UubMKBzJuLxUhgzOHwvOyU",
+  authDomain: "fir-proyect-972b9.firebaseapp.com",
+  projectId: "fir-proyect-972b9",
+  storageBucket: "fir-proyect-972b9.firebasestorage.app",
+  messagingSenderId: "987944057539",
+  appId: "1:987944057539:web:385dfddd3722891bc60076",
+  measurementId: "G-8JDE8TNMPG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export { app, auth, analytics };
